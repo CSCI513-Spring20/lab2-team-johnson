d6743503e506b5810cc2099a49e269a37c005e2e
Donald Johnson <donaldjamesjohnson@protonmail.com>
2020-02-04 10:09:02 -0600

    Fixed path

---
+++ src/Lab2_Package/OceanExplorer.java
@@ -0,0 +1,17 @@
+import javafx.scene.Scene;
+import javafx.application.Application;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.font;
+import javafx.scene.text.Text;
+import javafx.scene.stage.Stage;
+
+public class OceanExplorer extends Application{
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void start(Stage oceanStage) throw Exception{
+		AnchorPane anchorPane = new AnchorPane();
+	}
+	
+}
