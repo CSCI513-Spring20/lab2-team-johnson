0cf057ca3164c5efa301a159f01c1bf693cfbfc1
Donald Johnson <donaldjamesjohnson@protonmail.com>
2020-02-04 16:12:50 -0600

    Initial commit

---
+++ src/OceanExplorer.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class OceanExplorer extends Application {
+
+	public void start(Stage oceanStage) throws Exception {
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
