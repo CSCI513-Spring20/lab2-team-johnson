6dfd85378aea1498ab19ff58070e5a720386da1f
Donald Johnson <donaldjamesjohnson@protonmail.com>
2020-02-05 23:43:31 -0600

    issue3: created and drew ocean grid.

--- src/OceanExplorer.java
+++ src/OceanExplorer.java
@@ -1,18 +1,35 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class OceanExplorer extends Application {
-
+	
+	
 	public void start(Stage oceanStage) throws Exception {
+		final int dimension = 10;
+		final int scale = 50;
+		OceanMap oceanMap = new OceanMap();
+		boolean[][] oceanGrid = oceanMap.getMap();
 		AnchorPane myPane = new AnchorPane();
-		Scene scene = new Scene(myPane, 640, 480);
+		Scene scene = new Scene(myPane, scale*dimension, scale*dimension);
+		
 		oceanStage.setScene(scene);
 		oceanStage.setTitle("Christopher Columbus Game");
 		oceanStage.show();
+		
+		for (int x = 0; x < dimension; x++) {
+			for (int y = 0; y < dimension; y++) {
+				Rectangle rect = new Rectangle(x*scale, y*scale, scale, scale);
+				rect.setStroke(Color.BLACK);
+				rect.setFill(Color.PALETURQUOISE);
+				myPane.getChildren().add(rect);
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
